f5842f79dc1d8773d2533347a7fcba08f302985b
hua3698 <a29424315@gmail.com>
2021-02-05 14:06:37 +0800

    123

--- web02-2/back/que.php
+++ web02-2/back/que.php
@@ -15,12 +15,57 @@
         </table>
     </form>
 </fieldset>
+
+<fieldset>
+    <legend>問卷列表</legend>
+    <table>
+        <tr style="background:#eee" id="add">
+            <td>問卷名稱</td>
+            <td>投票數</td>
+            <td>開放</td>
+        </tr>
+        <?php
+        $que = $Que->all(['subject' => 0]);
+        foreach ($que as $q) {
+        ?>
+            <tr>
+                <td><?= $q['que']; ?></td>
+                <td><?= $q['count']; ?></td>
+                <td>
+                    <?php
+                    if ($q['sh'] == 1) {
+                    ?>
+                        <button type="button" onclick="que()" class="<?=$q['id'];?>">開放</button>
+                    <?php
+                    } else {
+                    ?>
+                        <button type="button" onclick="que()" class="<?=$q['id'];?>">隱藏</button
+                    <?php
+                    }
+                    ?>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+</fieldset>
+
 <script>
-    function more(){
+    function more() {
         $("#add").after(`
         <tr style="background:#eee">
                 <td colspan="2">選項<input type="text" name="option[]" id=""></td>
             </tr>
         `)
+    }
+
+    function que() {
+        // let word=$(this).html()
+        console.log($(this))
+        if (word == "開放") $(this).html("---")
+        else $(word).html("開放")
+        // $.get("api/good.php", {id})
+
     }
 </script>
